a635276e5dea9514a299a49ba63e7de43a4f9674
IvanHCenalmor <ivanhcenalmor@gmail.com>
2023-08-25 10:11:47 +0100

    Improved GUI

--- zenity.sh
+++ zenity.sh
@@ -1,55 +1,27 @@
 intro_window() {
-    zenity --question \
-                --title="Intro" \
-                --text="Information will appear and you need to give it." \
-                --ok-label="Next" \
-                --cancel-label="Cancel"
-    INTRO_RC=$?
-    if [ $INTRO_RC -eq 0 ]; then
-        config_window
+
+    # Mandatory arguments
+    if [ -z $CONFIG_PATH ]; then
+        config_output="-"
     else
-        exit 1
+        config_output=$CONFIG_PATH
     fi
-}
 
-config_window() {
-    CONFIG_PATH=$(zenity --file-selection \
-        --title "Select configuration.yaml" \
-        --file-filter="*.yaml")
-    CONFIG_RC=$?
-    if [ $CONFIG_RC -eq 0 ]; then
-        data_window
-    elif [ $CONFIG_RC -eq 1 ]; then
-        intro_window
+    if [ -z $DATA_PATH ]; then
+        data_output="-"
     else
-        exit 1
+        data_output=$DATA_PATH
     fi
-}
 
-data_window() {
-    DATA_PATH=$(zenity --file-selection \
-       --title "Select data_path" \
-       --directory)
-    DATA_RC=$?
-    if [ $DATA_RC -eq 0 ]; then
-        continue_window
-    elif [ $DATA_RC -eq 1 ]; then
-        config_window
-    else
-        exit 1
-    fi
-}
-
-continue_window() {
-
+    # Optional arguments
     if [ -z $GPU_FLAG_RC ]; then
         GPU_FLAG_RC=0
     fi
 
     if [ $GPU_FLAG_RC -eq 0 ]; then
-        gpu_output="No"
+        gpu_output="<span foreground='red'>No</span>" # "<b>No</No>"
     else
-        gpu_output="Yes"
+        gpu_output="<span foreground='green'>Yes</span>" #"<b>Yes</b>"
     fi
 
     if [ -z $NOTEBOOK_PATH ]; then
@@ -64,28 +36,65 @@ continue_window() {
         requirements_output=$REQUIREMENTS_PATH
     fi
 
-    extra_config_text="GPU: ${gpu_output}\nLocal notebook: ${notebook_output}\nLocal requirements: ${requirements_output}"
+    req_config_text="\tConfiguration file: <b>${config_output}</b>\n\tData folder: <b>${data_output}</b>"
+    extra_config_text="\t(optional) GPU: <b>${gpu_output}</b>\n\t(optional) Local notebook: <b>${notebook_output}</b>\n\t(optional) Local requirements: <b>${requirements_output}</b>"
 
     CONTINUE=$(zenity --info \
+       --no-wrap \
        --title="Continue" \
-       --text="Information will appear and you need to give it.\nExtra configurations:\n${extra_config_text}" \
-       --ok-label="Back" \
+       --text="<big><b>Welcome to DL4Mic_everywhere!</b></big>\nBellow you have the data you need to provide. The paths to both the configuration yaml file and data folder are mandatory.\nThe rest of them are optional, by default the GPU usage is set as <b>No</b> and if no local paths are specified to the notebook and\nrequirementes files, they will be downloaded using the URLs from the configuration yaml file.\n\nValues of the arguments: \n${req_config_text}\n${extra_config_text}\n" \
+       --ok-label="Cancel" \
+       --extra-button="Configuration file" \
+       --extra-button="Data folder" \
        --extra-button="GPU" \
        --extra-button="Local notebook" \
        --extra-button="Local requirements" \
        --extra-button="Done")
     CONTINUE_RC=$?
-    if [ "$CONTINUE" = "GPU" ]; then
+    if [ "$CONTINUE" = "Configuration file" ]; then
+        config_window
+    elif [ "$CONTINUE" = "Data folder" ]; then
+        data_window
+    elif [ "$CONTINUE" = "GPU" ]; then
         gpu_window
     elif [ "$CONTINUE" = "Local notebook" ]; then
         notebook_window
     elif [ "$CONTINUE" = "Local requirements" ]; then
         requirements_window
-    elif [ $CONTINUE_RC -eq 1 ] || [ "$CONTINUE" = "Done" ]; then
-        true
-    elif [ $CONTINUE_RC -eq 0 ]; then
-        data_window
+    elif [ "$CONTINUE" = "Done" ]; then
+        if [ -z $CONFIG_PATH ]; then
+            zenity --error \
+                   --text="You need to specify a path to the configuration file."
+            intro_window
+        elif [ -z $DATA_PATH ]; then
+            zenity --error \
+                   --text="You need to specify a path to the data folder."
+            intro_window
+        else
+            echo "Succes!!"
+            exit
+        fi
+    elif [ $CONTINUE_RC -eq 0 ] ||  [ $CONTINUE_RC -eq 1 ]; then
+        echo "OUT"
+        exit
+    else
+        echo "An unexpected error has occurred."
+        exit
     fi
+}
+
+config_window() {
+    CONFIG_PATH=$(zenity --file-selection \
+        --title "Select configuration.yaml" \
+        --file-filter="*.yaml")
+    intro_window
+}
+
+data_window() {
+    DATA_PATH=$(zenity --file-selection \
+       --title "Select data_path" \
+       --directory)
+    intro_window
 }
 
 gpu_window() {
@@ -95,24 +104,22 @@ gpu_window() {
         --ok-label="Yes" \
         --cancel-label="No"
     GPU_FLAG_RC=$?
-    echo $GPU_FLAG_RC
     GPU_FLAG_RC=$(expr 1 - $GPU_FLAG_RC )
-    echo $GPU_FLAG_RC
-    continue_window
+    intro_window
 }
 
 notebook_window() {
     NOTEBOOK_PATH=$(zenity --file-selection \
        --title "Select Notebook.ipynb" \
        --file-filter="*.ipynb")
-    continue_window
+    intro_window
 }
 
 requirements_window() {
     REQUIREMENTS_PATH=$(zenity --file-selection \
        --title "Select requirements.txt" \
        --file-filter="*.txt")
-    continue_window
+    intro_window
 }
 
 intro_window
