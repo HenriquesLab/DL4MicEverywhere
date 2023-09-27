e8e5778b5ff593d2b58353a32ce5e25b2b9fe4cb
Iván Hidalgo <47150974+IvanHCenalmor@users.noreply.github.com>
2023-09-27 17:57:58 +0100

    Add files via upload

--- gui.tcl
+++ gui.tcl
@@ -1,29 +1,58 @@
 #! /usr/local/bin/wish
 
-set width 350
+set width 400
 set height 250
 set width_offset [expr { ( [winfo vrootwidth  .] - $width  ) / 2 }]
 set height_offset [expr { ( [winfo vrootheight .] - $height ) / 2 }]
 
-set types {
-    {"All Source Files"     {.tcl .tk } }
-    {"Image Files"          {.gif .png .jpg} }
-    {"All files"            *}
+set yaml_types {
+    {"All Source Files"     {.yaml } }
 }
 
-proc onSelect { label } {
-    global types
-    set file [tk_getOpenFile -filetypes $types -parent .]
-    $label configure -text $file
+proc onSelectYaml { yaml_path } {
+    global yaml_types
+    set file [tk_getOpenFile -filetypes $yaml_types -parent .]
+    $yaml_path configure -text $file
 }
 
-label .l -text "..."
-place .l -x 20 -y 90
+proc onSelectfolder { folder_path } {
+    set file [tk_chooseDirectory -parent .]
+    $folder_path configure -text $file
+}
 
-button .b -text "Select a file" \
-        -command "onSelect .l"
-place .b -x 20 -y 30
+
+frame .fr
+pack .fr -fill both -expand 1
+
+frame .fr.pnl -relief raised -borderwidth 1
+pack .fr.pnl -fill both -expand 1
+
+
+ttk::button .fr.cb -text "Close" -command { exit 1 }
+pack .fr.cb -padx 5 -pady 5 -side right 
+
+ttk::button .fr.ok -text "OK" -command { exit 0 }
+pack .fr.ok -side right
+
+
+
+label .yp -text "....."
+place .yp -x 240 -y 30
+
+button .byp -text "Select configuration.yaml" \
+        -command "onSelectYaml .yp"
+place .byp -x 20 -y 30
+
+
+label .ldp -text "......."
+place .ldp -x 240 -y 60
+
+button .bdp -text "Select data folder" \
+        -command "onSelectfolder .ldp"
+place .bdp -x 20 -y 60
+
 
 
 wm title . "Center"
 wm geometry . ${width}x${height}+${width_offset}+${height_offset}
+
