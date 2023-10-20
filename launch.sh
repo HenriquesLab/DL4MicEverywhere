085398f3fb45579c7888c94fd942f4cba25b7174
IvanHCenalmor <ivanhcenalmor@gmail.com>
2023-10-20 13:54:45 +0200

    Small fix

--- launch.sh
+++ launch.sh
@@ -256,13 +256,8 @@ if [ "$local_notebook_flag" -eq 1 ]; then
 fi
 
 if [ "$local_requirements_flag" -eq 1 ]; then
-<<<<<<< HEAD
    cp $requirements_path $BASEDIR/requirements.txt
    requirements_path=./requirements.txt
-=======
-    cp $requirements_path $BASEDIR/requirements.txt
-    requirements_path=./requirements.txt
->>>>>>> GUI
 fi
 
 # Check if there is the errata in ~/.docker/config.json where credsStore should be credStore
