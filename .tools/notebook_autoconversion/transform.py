342fe2b14f95d4266fa13d20196919b837460a12
IvanHCenalmor <ivanhcenalmor@gmail.com>
2023-11-07 16:03:27 +0000

    Fix autoconversion (first remove, then update)

--- .tools/notebook_autoconversion/transform.py
+++ .tools/notebook_autoconversion/transform.py
@@ -27,11 +27,43 @@ def transform_nb(path_original_nb, path_new_nb, remove_sections=[]):
     cell_idx = 0
     ipywidget_imported = False
 
-    # Create a new notebook
+    # Create a new notebook to remove the sections
+    removed_sections_ntb = nbformat.v4.new_notebook()
+
+    # Go through each cell in the original notebook to get the sections localizers 
+    for cell in colab_nb.cells:
+        new_cells = []
+
+        # If the cell is a code cell
+        if cell.cell_type == "code":
+            code = cell.source
+            # Convert code to cells and track if ipywidgets is imported
+            new_cells, ipywidget_imported = code_to_cell(code, ipywidget_imported, function_name='function')
+
+        # If the cell is a markdown cell
+        elif cell.cell_type == "markdown":
+            text = cell.source
+            # Convert markdown to a cell
+            new_text, section_localizer = markdown_to_cell(text, section_localizer, cell_idx)
+            new_cells = [nbformat.v4.new_markdown_cell(new_text)]
+
+        # If new cells are created, add them to the new notebook
+        if new_cells:
+            cell_idx += len(new_cells)
+
+    # Remove specified sections from the markdown cells in the new notebook
+    removed_sections_ntb.cells, section_localizer = remove_section_list(cells=colab_nb.cells,
+                                                          section_localizer=section_localizer,
+                                                          section_list=remove_sections)
+
     new_nb = nbformat.v4.new_notebook()
 
+    section_localizer = {}
+    cell_idx = 0
+    ipywidget_imported = False
+
     # Iterate over each cell in the original notebook
-    for cell in colab_nb.cells:
+    for cell in removed_sections_ntb.cells:
         new_cells = []
 
         # If the cell is a code cell
@@ -51,11 +83,6 @@ def transform_nb(path_original_nb, path_new_nb, remove_sections=[]):
         if new_cells:
             new_nb.cells.extend(new_cells)
             cell_idx += len(new_cells)
-
-    # Remove specified sections from the markdown cells in the new notebook
-    new_nb.cells, section_localizer = remove_section_list(cells=new_nb.cells,
-                                                          section_localizer=section_localizer,
-                                                          section_list=remove_sections)
 
     # Save the new notebook to a file
     nbformat.write(new_nb, path_new_nb)
