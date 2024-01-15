fe114dc027ffd6310a71c2b8bbb8646b6f648aa7
IvanHCenalmor <ivanhcenalmor@gmail.com>
2024-01-15 18:12:02 +0000

    Fix CI to process updated configurations

--- .tools/update_zerocost_version.py
+++ .tools/update_zerocost_version.py
@@ -6,14 +6,14 @@ import pandas as pd
 
 from dict_parser import dict_dl4miceverywhere_to_version
 
-def main(notebook_name):
+def main(config_path):
+
+    # Extract the name of the notebook
+    notebook_name = config_path.split('/')[-2]
+
     # Read the versions of the notebooks
     all_notebook_versions = pd.read_csv("https://raw.githubusercontent.com/HenriquesLab/ZeroCostDL4Mic/master/Colab_notebooks/Latest_Notebook_versions.csv", dtype=str)
     zerocost_version = all_notebook_versions[all_notebook_versions["Notebook"] == dict_dl4miceverywhere_to_version[notebook_name]]['Version'].iloc[0]
-    
-    # Go through all the notebooks and check if the version is the same
-    dl4miceverywhere_notebooks_path = 'DL4MicEverywhere/notebooks/ZeroCostDL4Mic_notebooks'
-    config_path = os.path.join(dl4miceverywhere_notebooks_path, notebook_name, 'configuration.yaml')
     
     # Read the information from configuration file and modify values
     with open(config_path, 'r') as f:
