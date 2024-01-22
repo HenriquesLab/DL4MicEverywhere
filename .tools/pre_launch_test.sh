16e20aae9574d9a0d2d93b890d3ff86de2b5b375
IvanHCenalmor <ivanhcenalmor@gmail.com>
2024-01-22 22:30:06 +0000

    Added double click mac executable

--- .tools/pre_launch_test.sh
+++ .tools/pre_launch_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# This script checks for root access and Docker installation on Unix-like systems
-if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
-  # Verify if the script is run as root, which is required for Docker to function correctly
-  if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as root (by using sudo). Otherwise docker won't work properly."
-    echo "Make sure to use the -E flag to preserve the environment variables when using ssh and X11"
-    echo "E.g. sudo -E bash launch.sh"
-    exit 1
-  fi
-fi
+# # This script checks for root access and Docker installation on Unix-like systems
+# if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
+#   # Verify if the script is run as root, which is required for Docker to function correctly
+#   if [ "$EUID" -ne 0 ]; then
+#     echo "Please run this script as root (by using sudo). Otherwise docker won't work properly."
+#     echo "Make sure to use the -E flag to preserve the environment variables when using ssh and X11"
+#     echo "E.g. sudo -E bash launch.sh"
+#     exit 1
+#   fi
+# fi
 
 # Verify if Docker is installed on the system
 if ! command -v docker &> /dev/null; then
