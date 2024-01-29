020901c220219b1b69ce26f82f1249031874b528
IvanHCenalmor <ivanhcenalmor@gmail.com>
2024-01-29 13:30:12 +0000

    Change Docker installation on Mac

--- .tools/requirements_installation.sh
+++ .tools/requirements_installation.sh
@@ -75,12 +75,23 @@ if ! command -v docker &> /dev/null; then
             # Docker official website, to get the best experience, still recommends to install Rosetta
             # To install Rosetta 2 manually from the command line, run the following command:
             softwareupdate --install-rosetta --agree-to-license
+
+            # Download the latest ARM64 Docker installer
+            curl https://desktop.docker.com/mac/main/arm64/Docker.dmg -o /tmp/Docker.dmg
+        else
+            # Download the latest AMD64 Docker installer
+            curl https://desktop.docker.com/mac/main/amd64/Docker.dmg -o /tmp/Docker.dmg
         fi
 
-        # Install Docker using Homebrew
-        brew install docker --cask
-        # Start Docker engine
-        systemctl start docker
+        # Install Docker
+        hdiutil attach /tmp/Docker.dmg
+        /Volumes/Docker/Docker.app/Contents/MacOS/install
+        hdiutil detach /Volumes/Docker
+
+        # # Install Docker using Homebrew
+        # brew install docker --cask
+        # # Start Docker engine
+        # systemctl start docker
 
     elif [[ "$OSTYPE" == "msys*" ]]; then
         # Windows
