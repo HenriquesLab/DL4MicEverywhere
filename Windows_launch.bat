3b2ee5286fd188642c6a78c648e71a09f8457097
IvanHCenalmor <ivanhcenalmor@gmail.com>
2024-02-16 10:04:09 +0000

    Remove " from echo

--- Windows_launch.bat
+++ Windows_launch.bat
@@ -17,12 +17,12 @@ if not exist "C:\Program Files\Docker\Docker\Docker Desktop.exe" (
   :: echo x=msgbox("To ensure that all the changes are properly applied, please restart your computer. Then click the Windows_launcher again.",0,"Installation completed") >> %tmp%\tmp.vbs
   :: wscript %tmp%\tmp.vbs
   :: del %tmp%\tmp.vbs
-  echo ""
-  echo "----------------------------------------------------"
-  echo "Docker Desktop installation completed!"
-  echo "To ensure that all the changes are properly applied, please restart your computer." 
-  echo "Then click the Windows_launcher."
-  echo "----------------------------------------------------"
+  echo 
+  echo ------------------------------------------------------------------------------------
+  echo Docker Desktop installation completed!
+  echo To ensure that all the changes are properly applied, please restart your computer.
+  echo Then click the Windows_launcher.
+  echo ------------------------------------------------------------------------------------
   pause
   exit
 )
@@ -63,12 +63,12 @@ if %isubuntu%==0 (
   :: wscript %tmp%\tmp.vbs
   :: del %tmp%\tmp.vbs
   
-  echo ""
-  echo "----------------------------------------------------"
-  echo "Ubuntu in WSL installation completed!"
-  echo "To ensure that all the changes are properly applied, please restart your computer." 
-  echo "Then click the Windows_launcher."
-  echo "----------------------------------------------------"
+  echo 
+  echo ------------------------------------------------------------------------------------
+  echo Ubuntu in WSL installation completed!
+  echo To ensure that all the changes are properly applied, please restart your computer. 
+  echo Then click the Windows_launcher.
+  echo ------------------------------------------------------------------------------------
   pause
   exit
 ) else (
