1768b06bce7e74861751fd5dc96bcd0ca42d2a7f
Iván Hidalgo <ivanhcenalmor@gmail.com>
2024-11-05 16:01:25 +0000

    Update the docker cleaning system

--- .tools/bash_tools/pre_build_launch/clean_docker.sh
+++ .tools/bash_tools/pre_build_launch/clean_docker.sh
@@ -1,6 +1,13 @@
 echo "Releasing Docker memory ..."
 
-docker image prune -a -f # >/dev/null2>&1
+# This will remove:
+# - all stopped containers
+# - all networks not used by at least one container
+# - all dangling images
+# - unused build cache
+
+# This will only be applied to items older than 24 hours
+docker system prune -f --filter "until=24h" # >/dev/null2>&1
 
 echo ""
 echo "################################"
